75eef3733274266d8015486ca44ff508c466be71
gabyguerra2006738 <gaby.guerra.mtz@gmail.com>
2024-09-23 00:06:35 -0600

    Add files via upload

--- IoTAPP.js
+++ IoTAPP.js
@@ -1,101 +1,51 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
- 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
- 
-
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdKVLSNJyEYOFIT8PJAzDeJnGRF7aHMWw",
-  authDomain: "esp32iot-c7048.firebaseapp.com",
-  databaseURL: "https://esp32iot-c7048-default-rtdb.firebaseio.com",
-  projectId: "esp32iot-c7048",
-  storageBucket: "esp32iot-c7048.appspot.com",
-  messagingSenderId: "637606995653",
-  appId: "1:637606995653:web:91e09f53cc57281e5d72ac",
-  measurementId: "G-L82P2VYH2R"
+  apiKey: "AIzaSyBCUdVQ8VLQGODNvwOhAHJzIItfaXaNQMs",
+  authDomain: "examenmediocurso.firebaseapp.com",
+  databaseURL: "https://examenmediocurso-default-rtdb.firebaseio.com",
+  projectId: "examenmediocurso",
+  storageBucket: "examenmediocurso.appspot.com",
+  messagingSenderId: "990876938776",
+  appId: "1:990876938776:web:9093401dc23c50c4e0496d",
+  measurementId: "G-J693XVKD4V"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+// Obtener referencias a los elementos del DOM
 var IoTApp = document.getElementById('IoTApp');
 var dbRef = firebase.database();
-var GPIO=0;
-var ADC =0;
-var color = "";
- 
- 
-let dbGPIO = dbRef.ref("ESP32IoTApp/GPIO/");
-dbGPIO.on('value', function(snapshot) {
-  console.log("El valor del GPIO es", snapshot.val());
-  GPIO=snapshot.val();
-document.getElementById("GPIO_0_Id").innerHTML  = "GPIO" + " " + "=" + GPIO;
+var temperatura = 0;
+var altura = 0;
+var presion = 0;
+
+// Referencias a los datos en Firebase
+let dbtemperatura = dbRef.ref("ESP32IoTApp/temperatura/");
+dbtemperatura.on('value', function(snapshot) {
+  console.log("El valor del temperatura es", snapshot.val());
+  temperatura = snapshot.val();
+  document.getElementById("temperaturaId").innerHTML = "temperatura = " + temperatura + "°C";
 });
- 
-let dbADC = dbRef.ref("ESP32IoTApp/ADC/");
-dbADC.on('value', function(snapshot) {
-  console.log("El valor del ADC es", snapshot.val());
-  ADC=snapshot.val();
-document.getElementById("ADCId").innerHTML  = "ADC" + " " + "=" + ADC;
+
+let dbpresion = dbRef.ref("ESP32IoTApp/presion/");
+dbpresion.on('value', function(snapshot) {
+  console.log("El valor del presion es", snapshot.val());
+  presion = snapshot.val();
+  document.getElementById("presionId").innerHTML ="presion = " + presion + "mmHg";
 });
- 
-let dbLedStatus= dbRef.ref("ESP32IoTApp/Led_Control/estadoLC");
- 
-$(document).ready(function() {
-            // Manejar clic en el botón de encender/apagar
-            $("#toggleBtn").click(function(){
-                // Obtener el estado actual del botón
-                var estadoActual = $(this).text();
- 
-                // Cambiar el estado del botón
-                var nuevoEstado = (estadoActual === "Encendido") ? "Apagado" : "Encendido";
-                $(this).text(nuevoEstado);
- 
-                // Enviar el nuevo estado al servidor o base de datos
-                enviarEstado(nuevoEstado);
-            });
- 
-             // Función para enviar el estado al servidor o base de datos
-            function enviarEstado(estado) {
-                // Puedes hacer una solicitud al servidor aquí o realizar cualquier acción necesaria
-                console.log("Nuevo estado enviado: " + estado);
-                dbLedStatus.set(estado);
-            }
- 
-        });
- 
-let dbDAC = dbRef.ref("ESP32IoTApp/DAC/");
- 
-        document.addEventListener('DOMContentLoaded', function() {
-            // Agregar un evento de clic al botón
-            document.getElementById('capturarBtnDAC').addEventListener('click', function() {
-                // Obtener el valor del campo de entrada
-                var numeroCapturado = document.getElementById('InputDAC').value;
- 
-                // Verificar si se ingresó un número
-                if (!isNaN(numeroCapturado)) {
-                    // Mostrar el número en la consola
-                    console.log("Número capturado: " + numeroCapturado);
-                    dbDAC.set(numeroCapturado);
- 
-                } else {
-                    // Mostrar un mensaje si no se ingresó un número válido
-                    console.log("Por favor, ingresa un número válido.");
-                }
-            });
-        });
- 
- 
-let dbLedRGB = firebase.database().ref("ESP32IoTApp/LedRGB/color");
- 
-$(document).ready(function() {
-            $("#guardarColor").click(function(){
-                // Obtener el valor del botón de radio seleccionado
-                var color = $("input[name='gridRadios']:checked").val();
- 
-                // Hacer algo con el color, como guardarlo en tu base de datos
-                console.log("Color seleccionado: " + color);
-                // Hacer algo con el color, como guardarlo en tu base de datos
-               dbLedRGB.set(color);
-            });
-        });
+
+let dbaltura = dbRef.ref("ESP32IoTApp/altura/");
+dbaltura.on('value', function(snapshot) {
+  console.log("El valor del altura es", snapshot.val());
+  altura = snapshot.val();
+  document.getElementById("alturaId").innerHTML = "altura = " + altura + "m";
+});
+
+
+
+   
+
+
+
